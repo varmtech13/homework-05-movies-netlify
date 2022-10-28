928ba2336b96f4b4cb11bda91f8f9f672e8dfae5
varmtech13 <varmtech.co@gmail.com>
2022-10-28 10:37:40 +0200

    Fix

--- src/Hooks/usePages.js
+++ src/Hooks/usePages.js
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { useNavigate, useLocation, useSearchParams } from 'react-router-dom';
+import { useNavigate, useLocation } from 'react-router-dom';
 
 export default function usePages() {
   const [totalPage, setTotalPage] = useState(1);
