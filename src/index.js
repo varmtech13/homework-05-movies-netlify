5d1bb95a59d7c477f27383aef37e8f36a16d3adb
varmtech13 <varmtech.co@gmail.com>
2022-10-30 19:40:02 +0100

    Fix for netlify

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const root = createRoot(rootElement);
 
 root.render(
   <StrictMode>
-    <BrowserRouter basename="/homework-05-movies/">
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </StrictMode>
